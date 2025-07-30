2b07e718cf35ab905db86c2604e1b3bd1e4ac1b9
A1nz2802 <a1nzdev28@gmail.com>
2025-07-30 00:33:07 -0500

    add neoscroll plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -75,6 +75,25 @@ return {
     },
   },
 
+  {
+    "karb94/neoscroll.nvim",
+    opts = {
+      hide_cursor = true,          
+      stop_eof = true,             
+      respect_scrolloff = false,                           
+      cursor_scrolls_alone = true, 
+      duration_multiplier = 0.5,
+      easing = 'linear',
+      pre_hook = nil,
+      post_hook = nil,
+      performance_mode = false,
+      ignored_events = {
+          'WinScrolled', 'CursorMoved'
+      },
+    },
+    lazy = false,
+  }
+
   --[[ {
     "saghen/blink.cmp",
     optional = true,
